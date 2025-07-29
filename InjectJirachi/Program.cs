95fe3e3855c72090586a591ae84f8ccf3c1072db
aestellic <aestellic@protonmail.com>
2025-07-29 10:15:46 -0400

    add --fill-party flag

--- InjectJirachi/Program.cs
+++ InjectJirachi/Program.cs
@@ -7,31 +7,12 @@ namespace InjectJirachi
 {
     class Program
     {
-        static void Main(string[] args)
+        static int InjectJirachiToSave(SAV3 sav3, Random rng)
         {
-            if (args.Length < 2)
-            {
-                Console.WriteLine("Usage: InjectJirachi <input.sav> <output.sav>");
-                return 2;
-            }
-
-            string inputSavePath = args[0];
-            string outputSavePath = args[1];
-
-            byte[] savData = File.ReadAllBytes(inputSavePath);
-
-            var sav = SaveUtil.GetVariantSAV(savData);
-            if (sav is not SAV3 sav3)
-            {
-                Console.WriteLine("Not a Gen 3 save file.");
-                return 2;
-            }
-
-            Console.WriteLine($"Game: {sav.Version}");
-
             PK3 pk;
-            ushort randomSeed = (ushort)new Random().Next(0x10000);
+            ushort randomSeed = (ushort)rng.Next(0x10000);
             Console.WriteLine($"Seed: 0x{randomSeed:X4}");
+
             try
             {
                 pk = GenJirachi.Program.CreateWishmakerJirachi(randomSeed);
@@ -51,7 +32,7 @@ namespace InjectJirachi
 
             if (party.Count <= maxPartySize)
             {
-                openSlot = party.Count;  // next empty slot is at count index
+                openSlot = party.Count;
             }
             else
             {
@@ -59,13 +40,46 @@ namespace InjectJirachi
                 return 1;
             }
 
-            sav.SetPartySlotAtIndex(pk, openSlot);
+            sav3.SetPartySlotAtIndex(pk, openSlot);
+            Console.WriteLine($"Injected into party slot {openSlot + 1}.");
+            return 0;
+        }
 
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: InjectJirachi <input.sav> <output.sav> [--fill-party]");
+                return 2;
+            }
+
+            string inputSavePath = args[0];
+            string outputSavePath = args[1];
+            bool fillParty = Array.Exists(args, arg => arg == "--fill-party");
+
+            byte[] savData = File.ReadAllBytes(inputSavePath);
+            var sav = SaveUtil.GetVariantSAV(savData);
+            if (sav is not SAV3 sav3)
+            {
+                Console.WriteLine("Not a Gen 3 save file.");
+                return 2;
+            }
+
+            Console.WriteLine($"Game: {sav.Version}");
+
+            var rng = new Random();
+            int result;
+            do
+            {
+                result = InjectJirachiToSave(sav3, rng);
+            }
+            while (fillParty && result == 0);
 
             byte[] newSave = sav3.Write();
             File.WriteAllBytes($"{outputSavePath}", newSave);
+            Console.WriteLine($"Saved as {outputSavePath}");
 
-            Console.WriteLine($"Injected into party slot {openSlot + 1}. Saved as {outputSavePath}");
+            return result;
         }
     }
 }
--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 Injects a legal randomized Wishmaker Jirachi into any mainline Generation III Save File. Intended for shiny hunting a legal Wishmaker Jirachi using custom distribution hardware (i.e. Raspberry Pi alongside a GBxCart RW).
 
 ## Usage
-`InjectJirachi <input.sav> <output.sav>`
+`InjectJirachi <input.sav> <output.sav> [--fill-party]`
 
 ## Notes
  - You must have an open slot in your party; the program will exit if you do not.
  - The seed used to generate the Jirachi is randomized, so standard Wishmaker RNG manipulation will not work.
- - Unlimited Jirachi's can be received per save
+ - Unlimited Jirachi's can be received per save.
+ - `--fill-party` will fill all open slots in the party with Jirachi's.
